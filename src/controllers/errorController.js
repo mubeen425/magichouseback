c195a80554e12f89d22f7fa6a62edd4791984924
Hasnat Shabbir <96876568+hasnat-hj@users.noreply.github.com>
2023-10-13 06:44:03 +0500

    error route updated

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -52,8 +52,7 @@ const sendErrorDev = (err, req, res) => {
 };
 
 const sendErrorProd = (err, req, res) => {
-  // A) API
-  if (req.originalUrl.startsWith('/api')) {
+
     // A) Operational, trusted error: send message to client
     if (err.isOperational) {
       return res.status(err.statusCode).send({
@@ -69,24 +68,9 @@ const sendErrorProd = (err, req, res) => {
       status: 'error',
       message: 'Something went very wrong!'
     });
-  }
+  
 
-  // B) RENDERED WEBSITE
-  // A) Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong!',
-      msg: err.message
-    });
-  }
-  // B) Programming or other unknown error: don't leak error details
-  // 1) Log error
-  console.error('ERROR 💥', err);
-  // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: 'Please try again later.'
-  });
+  
 };
 
 module.exports = (err, req, res, next) => {
